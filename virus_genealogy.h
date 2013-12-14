537c65da01ab97f97f0afeb3e472f62d75573983
Radosław Piórkowski <radek25c@gmail.com>
2013-12-14 15:57:51 +0100

    Add: Makefile, test.cc; Fix: Minor fixes

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -1,14 +1,19 @@
 #ifndef _VIRUS_GENEALOGY_
 #define _VIRUS_GENEALOGY_
 
-#include "virus.h"
-
 template <class Virus>
 class VirusGenealogy {
+public:
 	// Tworzy nową genealogię.
 	// Tworzy także węzęł wirusa macierzystego o identyfikatorze stem_id.
 	VirusGenealogy(typename Virus::id_type const &stem_id);
 
 };
+
+
+template <class Virus>
+VirusGenealogy<Virus>::VirusGenealogy(typename Virus::id_type const &stem_id) {
+	// TODO
+}
 
 #endif
